a6c1c7b0879327f02bfce76b3f3aebaa027cd4ef
meh <meh@schizofreni.co>
2013-04-17 18:47:10 +0200

    Add package.exs

---
+++ package.exs
@@ -0,0 +1,9 @@
+Expm.Package.new(
+  name: "benchmark",
+  description: "Simple module to benchmark fragments of code.",
+  version: "0.0.1",
+  licenses: [[name: "WTFPL"]],
+  keywords: ["benchmark", "utility"],
+  maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
+  repositories: [[github: "meh/elixir-benchmark", tag: "v0.0.1"]]
+)
